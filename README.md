44c5904c079f4aa22349216ea4dd3daa9973bb5f
Phoebe Goldman <phoebe@goldman-tribe.org>
2025-03-03 14:23:15 -0500

    v2 of the module code, with the new feature implemented compatibly

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ This is a [SpacetimeDB](https://spacetimedb.com/) demo project intended to showc
 
 This repository has three branches:
 
-- `v1` (current) shows the initial version of the module, before implementing a new feature which requires an added column.
+- `v1` shows the initial version of the module, before implementing a new feature which requires an added column.
 - `fails-publish` shows a version of the module which implements the new feature, but will fail to publish on top of `v1` due to an incompatible schema change.
-- `v2` shows a version of the module which implements the new feature, and can cleanly publish on top of `v1` without clearing the database or running a manual migration.
+- `v2` (current) shows a version of the module which implements the new feature, and can cleanly publish on top of `v1` without clearing the database or running a manual migration.
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,16 @@ struct Character {
     class: Class,
 }
 
+#[spacetimedb::table(name = character_v2, public)]
+struct CharacterV2 {
+    #[primary_key]
+    player_id: Identity,
+    nickname: String,
+    level: u32,
+    class: Class,
+    alliance: Alliance,
+}
+
 #[derive(SpacetimeType, Debug, Copy, Clone)]
 enum Class {
     Fighter,
@@ -16,27 +26,67 @@ enum Class {
     Medic,
 }
 
+#[derive(SpacetimeType, Debug, Copy, Clone)]
+enum Alliance {
+    Good,
+    Neutral,
+    Evil,
+}
+
 #[spacetimedb::reducer]
 fn create_character(ctx: &ReducerContext, class: Class, nickname: String) {
-    log::info!("Creating new level 1 {class:?} named {nickname}",);
+    log::info!(
+        "Creating new level 1 {class:?} named {nickname} for player {}",
+        ctx.sender,
+    );
+
     ctx.db.character().insert(Character {
         player_id: ctx.sender,
         nickname: nickname.clone(),
         level: 1,
         class,
     });
+
+    ctx.db.character_v2().insert(CharacterV2 {
+        player_id: ctx.sender,
+        nickname,
+        level: 1,
+        class,
+        alliance: Alliance::Neutral,
+    });
 }
 
-fn find_character_for_player(ctx: &ReducerContext) -> Character {
-    ctx.db
+fn find_character_for_player(ctx: &ReducerContext) -> CharacterV2 {
+    if let Some(character) = ctx.db.character_v2().player_id().find(ctx.sender) {
+        // Already migrated; just return the new player.
+        return character;
+    }
+
+    // Not yet migrated; look up an old character and update it.
+    let old_character = ctx
+        .db
         .character()
         .player_id()
         .find(ctx.sender)
-        .expect("Player has not created a character")
+        .expect("Player has not created a character");
+
+    ctx.db.character_v2().insert(CharacterV2 {
+        player_id: old_character.player_id,
+        nickname: old_character.nickname,
+        level: old_character.level,
+        class: old_character.class,
+        alliance: Alliance::Neutral,
+    })
 }
 
-fn update_character(ctx: &ReducerContext, character: Character) {
-    ctx.db.character().player_id().update(character);
+fn update_character(ctx: &ReducerContext, character: CharacterV2) {
+    ctx.db.character().player_id().update(Character {
+        player_id: character.player_id,
+        nickname: character.nickname.clone(),
+        level: character.level,
+        class: character.class,
+    });
+    ctx.db.character_v2().player_id().update(character);
 }
 
 #[spacetimedb::reducer]
@@ -45,7 +95,7 @@ fn rename_character(ctx: &ReducerContext, new_name: String) {
     log::info!("Renaming {} to {}", character.nickname, new_name,);
     update_character(
         ctx,
-        Character {
+        CharacterV2 {
             nickname: new_name,
             ..character
         },
@@ -63,8 +113,25 @@ fn level_up_character(ctx: &ReducerContext) {
     );
     update_character(
         ctx,
-        Character {
+        CharacterV2 {
             level: character.level + 1,
+            ..character
+        },
+    );
+}
+
+#[spacetimedb::reducer]
+fn choose_alliance(ctx: &ReducerContext, alliance: Alliance) {
+    let character = find_character_for_player(ctx);
+    log::info!(
+        "Setting alliance of {} to {:?}",
+        character.nickname,
+        alliance,
+    );
+    update_character(
+        ctx,
+        CharacterV2 {
+            alliance,
             ..character
         },
     );
